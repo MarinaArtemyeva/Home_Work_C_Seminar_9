5b883683d0667291faa0848848b846ec23a05707
Marina Artemyeva <martemyeva@yahoo.com>
2023-03-10 18:25:24 +0600

    Add code in TASK003

--- TASK003/Program.cs
+++ TASK003/Program.cs
@@ -2,3 +2,22 @@
 //Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+
+int m = Prompt("Введите M: ");
+int n = Prompt("Введите N: ");
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
